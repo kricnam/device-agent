626f3e476c84a93e5beea0a9ff4023be7bdc7ed8
me.xinxin <me.xinxin@163bffd8-f3b6-11de-82c3-25da7dca7fdf>
2009-12-28 13:58:16 +0000

    init

---
+++ device-agent.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : device-agent.cpp
+// Author      : Meng Xinxin
+// Version     :
+// Copyright   : No Warrinty, No Right reserved
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!!!");
+	return EXIT_SUCCESS;
+}
