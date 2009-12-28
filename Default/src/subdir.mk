b224dcba4eb63b3f88eafbdbcf1394159fc9730d
me.xinxin <me.xinxin@163bffd8-f3b6-11de-82c3-25da7dca7fdf>
2009-12-28 15:11:58 +0000

    init

--- Default/src/subdir.mk
+++ Default/src/subdir.mk
@@ -4,12 +4,24 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../src/CRC16.cpp \
+../src/CommunicationCommand.cpp \
+../src/Packet.cpp \
+../src/SerialPort.cpp \
 ../src/device-agent.cpp 
 
 OBJS += \
+./src/CRC16.o \
+./src/CommunicationCommand.o \
+./src/Packet.o \
+./src/SerialPort.o \
 ./src/device-agent.o 
 
 CPP_DEPS += \
+./src/CRC16.d \
+./src/CommunicationCommand.d \
+./src/Packet.d \
+./src/SerialPort.d \
 ./src/device-agent.d 
 
 
