e74fe22e31cee7eaaad15da1fc8141c1b7032b37
me.xinxin <me.xinxin@163bffd8-f3b6-11de-82c3-25da7dca7fdf>
2009-12-30 13:35:29 +0000

    init

---
+++ src/Channel.cpp
@@ -0,0 +1,24 @@
+/*
+ * Channel.cpp
+ *
+ *  Created on: 2009-12-29
+ *      Author: mxx
+ */
+
+#include "Channel.h"
+
+namespace bitcomm
+{
+
+Channel::Channel()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Channel::~Channel()
+{
+	// TODO Auto-generated destructor stub
+}
+
+}
---
+++ src/Channel.h
@@ -0,0 +1,23 @@
+/*
+ * Channel.h
+ *
+ *  Created on: 2009-12-29
+ *      Author: mxx
+ */
+
+#ifndef CHANNEL_H_
+#define CHANNEL_H_
+
+namespace bitcomm
+{
+
+class Channel
+{
+public:
+	Channel();
+	virtual ~Channel();
+};
+
+}
+
+#endif /* CHANNEL_H_ */
