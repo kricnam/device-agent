08443ae4cd7c5069f1e7eb6cc788b375de923b06
me.xinxin <me.xinxin@163bffd8-f3b6-11de-82c3-25da7dca7fdf>
2010-02-01 13:44:11 +0000

    init

--- Default/src/device-agent.d
+++ Default/src/device-agent.d
@@ -1,6 +1,6 @@
 src/device-agent.d src/device-agent.o: ../src/device-agent.cpp \
  ../src/DataTask.h ../src/SerialPort.h ../src/Channel.h ../src/Modem.h \
- ../src/Protocol.h ../src/PacketQueue.h ../src/Packet.h ../src/TCPPort.h \
+ ../src/TCPPort.h ../src/Protocol.h ../src/PacketQueue.h ../src/Packet.h \
  ../src/CommunicationCommand.h ../src/HistoryDataRequestCmd.h \
  ../src/ControlTask.h ../src/Config.h ../src/DebugLog.h
 
@@ -12,13 +12,13 @@ src/device-agent.d src/device-agent.o: ../src/device-agent.cpp \
 
 ../src/Modem.h:
 
+../src/TCPPort.h:
+
 ../src/Protocol.h:
 
 ../src/PacketQueue.h:
 
 ../src/Packet.h:
-
-../src/TCPPort.h:
 
 ../src/CommunicationCommand.h:
 
