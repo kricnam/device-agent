89643443ec2c83527def5b918ef5a373affe6876
me.xinxin <me.xinxin@163bffd8-f3b6-11de-82c3-25da7dca7fdf>
2010-02-10 09:40:07 +0000

    init

--- src/Modem.cpp
+++ src/Modem.cpp
@@ -9,8 +9,10 @@
 #include "DebugLog.h"
 #include "TCPPort.h"
 #include "DebugLog.h"
+#include "Config.h"
 #include <sys/time.h>
 #include <stdlib.h>
+#include <stdio.h>
 /* MODEM_H_ */
 //send out Date,Time and Title message
 namespace bitcomm
@@ -39,32 +41,51 @@ void Modem::PowerOn(void)
 
 		try
 		{
-		if (UT_ATPort.Open() < 0)
-		{
-			ERROR("fail to connect to UT");
-			break;
-		}
+			int retry=3;
+			do
+			{
+				UT_ATPort.SetTimeOut(5000000);
 
-		if (!UT_PowerOn())
-		{
-			ERROR("fail to active  UT");
-			break;
-		}
+				if (UT_ATPort.Open())
+				{
+					INFO("Connected to AT command port");
+					break;
+				}
+				ERROR("fail to connect to UT");
+			} while(--retry);
+			if (retry==0) break;
 
-		if (!AttachNet())
-		{
-			ERROR("Attach fail:%s",strCache.c_str());
-			break;
-		}
+			if (!UT_PowerOn())
+			{
+				ERROR("fail to active  UT");
+				break;
+			}
 
-		if (!EstablishIPConnection())
-		{
-			ERROR("fail %s",strCache.c_str());
-			break;
-		}
+			if (!AttachNet())
+			{
+				ERROR("Attach fail:%s",strCache.c_str());
+				break;
+			}
 
-		system("udhcpc -n -q");
-		return;
+			if (!LogonIP())
+			{
+				ERROR("fail to set context:%s",strCache.c_str());
+				break;
+			}
+
+			if (!CheckContext())
+			{
+				ERROR("fail to check context:%s",strCache.c_str());
+				break;
+			}
+
+			if (!ConnectIP())
+			{
+				ERROR("fail to connect:%s ",strCache.c_str());
+				break;
+			}
+			//system("udhcpc -n -q");
+			return;
 		}
 		catch(ChannelException& e)
 		{
@@ -75,42 +96,72 @@ void Modem::PowerOn(void)
 	return;
 }
 
-int Modem::EstablishIPConnection(void)
+int Modem::LogonIP(void)
 {
-	UT_ATPort.Write("AT+CGDCONT=10,\"IP\",\"bgan.inmarsat.com\"\n",39);
-	WaitATResponse("OK");
+	char buf[256]={0};
+	Config config(CONF_FILENAME);
+	snprintf(buf,256,"AT+CGDCONT=10,\"IP\",\"%s\",\"%s\",0,0,\"%s\",\"%s\"\n",
+			config.GetAPN().c_str(),config.GetIP().c_str(),
+			config.GetUserName().c_str(),
+			config.GetPassword().c_str());
+
+	int retry=3;
+	do
+	{
+		UT_ATPort.Write(buf,strlen(buf));
+	    if (WaitATResponse("OK"))
+	    	return 1;
+	}
+	while(--retry);
+	return 0;
+}
+
+int Modem::CheckContext(void)
+{
+	int retry=3;
+	Config config(CONF_FILENAME);
+	do
+	{
+		UT_ATPort.Write("AT+CGDCONT?\n",12);
+		if (WaitATResponse("OK"))
+		{
+			if (strCache.find(config.GetAPN())!=string::npos
+					&& strCache.find(config.GetIP())!=string::npos)
+			return 1;
+		}
+	}while(--retry);
+	return 0;
+
 	UT_ATPort.Write("AT_IAPPPCONNECT=1,10\n",21);
 	return WaitATResponse("OK");
+
 }
+
+int Modem::ConnectIP(void)
+{
+	int retry=3;
+	Config config(CONF_FILENAME);
+	do
+	{
+		UT_ATPort.Write("AT_IAPPPCONNECT=1,10\n",21);
+		if (WaitATResponse("OK",config.GetModemDelay()))
+			return 1;
+	}while(--retry);
+	return 0;
+}
+
 
 int Modem::UT_PowerOn()
 {
 	int ret = 0;
-	struct timeval start, now, diff;
-	UT_ATPort.Write("AT_ISIG=1\n", 10);
-	if (!WaitATResponse("OK"))
-	{
-		ERROR("UT response time out");
-		return 0;
-	}
-	gettimeofday(&start, 0);
+	int retry=3;
+	Config config("./agent.conf");
+	int waitTime = config.GetModemDelay();
 	do
 	{
-		int n = GetSignalLevel();
-		if (n > 40)
-		{
-			UT_ATPort.Write("AT_IPOINT=1\n",13);
-			WaitATResponse("OK");
-			ret = 1;
-			break;
-		}
-		gettimeofday(&now, 0);
-		timersub(&now,&start,&diff);
-		if (diff.tv_sec > 10)
-			break;
-	} while (1);
-	UT_ATPort.Write("AT_ISIG=0\n", 10);
-	WaitATResponse("OK");
+		UT_ATPort.Write("AT_IPOINT=1\n",13);
+		ret = WaitATResponse("OK",waitTime);
+	} while (--retry);
 	return ret;
 }
 
@@ -123,25 +174,34 @@ void Modem::PowerOff(void)
 {
 	INFO("Power off");
 	bPowerOff = true;
+	int retry=3;
 	do
 	{
 		try
 		{
-			UT_ATPort.Open();
-			UT_ATPort.Write("AT_IAPPPCONNECT=0\n",19);
-			WaitATResponse("OK");
+			//UT_ATPort.Open();
+			do
+			{
+				UT_ATPort.Write("AT_IAPPPCONNECT=0\n",19);
+				if (WaitATResponse("OK"))
+					break;
+			} while(--retry);
 			break;
-		} catch (ChannelException& e)
+		}
+		catch (ChannelException& e)
 		{
 			ERROR(e.what());
 		}
-	} while (1);
+	} while (--retry);
 
-	system("udhcpc -n -q");
-	system("ifconfig eth0 down");
+	//system("udhcpc -n -q");
+	if (system("ifconfig eth0 down"))
+	{
+		ERRTRACE();
+	}
 }
 
-int Modem::WaitATResponse(const char *szWait, bool bClear)
+int Modem::WaitATResponse(const char *szWait, int timeout,bool bClear)
 {
 	char buf[256];
 	if (bClear)
@@ -165,15 +225,21 @@ int Modem::WaitATResponse(const char *szWait, bool bClear)
 		}
 		gettimeofday(&now, 0);
 		timersub(&now,&start,&diff);
-		if (diff.tv_sec > 3)
+		if (diff.tv_sec > timeout)
 			return 0;
 	} while (1);
 }
 
 int Modem::AttachNet()
 {
-	UT_ATPort.Write("AT+CGATT?\n",10);
-	return WaitATResponse("+CGATT: 1,3");
+	int retry=3;
+	do
+	{
+		UT_ATPort.Write("AT+CGATT?\n",10);
+		if (WaitATResponse("+CGATT:1,3"))
+			return 1;
+	}while(--retry);
+	return 0;
 }
 
 float Modem::GetSignalLevel(void)
