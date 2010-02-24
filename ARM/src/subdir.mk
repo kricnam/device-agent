d38d4ab8797cec8f3367da4db57f39ae099b619c
me.xinxin <me.xinxin@163bffd8-f3b6-11de-82c3-25da7dca7fdf>
2010-02-24 16:03:28 +0000

    init

---
+++ ARM/src/subdir.mk
@@ -0,0 +1,69 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/CRC16.cpp \
+../src/Channel.cpp \
+../src/CommunicationCommand.cpp \
+../src/Config.cpp \
+../src/ControlTask.cpp \
+../src/DataTask.cpp \
+../src/DebugLog.cpp \
+../src/HistoryDataRequestCmd.cpp \
+../src/MPHealthCheckCmdPacket.cpp \
+../src/Modem.cpp \
+../src/Packet.cpp \
+../src/PacketQueue.cpp \
+../src/Protocol.cpp \
+../src/SerialPort.cpp \
+../src/TCPPort.cpp \
+../src/device-agent.cpp 
+
+OBJS += \
+./src/CRC16.o \
+./src/Channel.o \
+./src/CommunicationCommand.o \
+./src/Config.o \
+./src/ControlTask.o \
+./src/DataTask.o \
+./src/DebugLog.o \
+./src/HistoryDataRequestCmd.o \
+./src/MPHealthCheckCmdPacket.o \
+./src/Modem.o \
+./src/Packet.o \
+./src/PacketQueue.o \
+./src/Protocol.o \
+./src/SerialPort.o \
+./src/TCPPort.o \
+./src/device-agent.o 
+
+CPP_DEPS += \
+./src/CRC16.d \
+./src/Channel.d \
+./src/CommunicationCommand.d \
+./src/Config.d \
+./src/ControlTask.d \
+./src/DataTask.d \
+./src/DebugLog.d \
+./src/HistoryDataRequestCmd.d \
+./src/MPHealthCheckCmdPacket.d \
+./src/Modem.d \
+./src/Packet.d \
+./src/PacketQueue.d \
+./src/Protocol.d \
+./src/SerialPort.d \
+./src/TCPPort.d \
+./src/device-agent.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	/usr/local/arm/4.3.2/bin/arm-none-linux-gnueabi-g++ -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
